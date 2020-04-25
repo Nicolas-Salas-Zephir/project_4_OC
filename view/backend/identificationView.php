35c26856535cc944a408111eaff9c4cb0ff4112d
Nicolas Salas-Zephir <znco93@hotmail.fr>
2020-04-25 20:17:47 +0200

    Change the unit of measurement

--- view/backend/identificationView.php
+++ view/backend/identificationView.php
@@ -7,7 +7,7 @@ ob_start();
     <div class="identification d-flex flex-column justify-content-center">
         <div class="row  mb-5">
             <div class="col-lg-12 d-flex justify-content-center ">
-                <h1>LOGIN</h1>
+                <h1>Se connecter</h1>
             </div>
             <div class="col-lg-12 d-flex justify-content-center ">
                 <p><a href="index.php">Retour à la page d'accueil</a></p>
@@ -15,7 +15,7 @@ ob_start();
         </div>
 
         <div class="row d-flex justify-content-center ">
-            <div class="col-lg-4 ">
+            <div class="col-md-4 ">
                 <form action="index.php?action=addPost" method="post">
                     <div class="form-group">
                         <input type="text" class="form-control form-control-lg"  id="username" name="username" placeholder="Votre nom"required/>
@@ -24,7 +24,7 @@ ob_start();
                         <input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Mot de passe"required/>
                     </div>
                     <div class="d-flex justify-content-center mt-5">
-                        <button type="submit" class="btn btn-primary btn-lg btn-block">Envoyer</button>
+                        <button type="submit" class="btn btn-primary btn-sm btn-block">Valider</button>
                     </div>
                 </form>
             </div>
