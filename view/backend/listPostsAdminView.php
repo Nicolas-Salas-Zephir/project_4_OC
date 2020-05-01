d2a7e1cf730ad14f1ab12566e95d4b9302b2bbc2
Nicolas Salas-Zephir <znco93@hotmail.fr>
2020-05-01 18:40:37 +0200

    Center the text in the items

--- view/backend/listPostsAdminView.php
+++ view/backend/listPostsAdminView.php
@@ -13,21 +13,19 @@ while ($data = $posts->fetch())
         <div class="col-sm-12 ">
             <div id="post-id" class="post post-backend mb-5 mt-5">
                 <div class="post-main">
-                    <div class="post-content">
-                        <div class="row">
-                            <div class="col-sm-10">
-                                <p><i class="fas fa-user mr-3"></i><?= $data['author']; ?></p>
-                                <h2><?= $data['title']; ?></h2>
-                                <p><?php
-                                        $rest = substr($data['content'], 0, 300); 
-                                        echo $rest;
-                                    ?></p>
-                            </div>
-                            <div class="col-sm-2 d-flex align-items-center justify-content-center">
-                                <!-- <a href="index.php?action=deletePost&id=<?= $data['id']; ?>"><i class="far fa-trash-alt text-danger mr-2"></i></a>
-                                <a href="index.php?action=postAdmin&id=<?= $data['id']; ?>"><i class="far fa-edit text-primary"></i></a> -->
-                                <a href="index.php?action=postAdmin&id=<?= $data['id']; ?>"><i class="far fa-eye text-info"></i></a>
-                            </div>
+                    <div class="post-content d-flex flex-row justify-content-between align-items-center">
+                        <div class="post-main__content">
+                            <p><i class="fas fa-user mr-3"></i><?= $data['author']; ?></p>
+                            <h2><?= $data['title']; ?></h2>
+                            <p><?php
+                                    $rest = substr($data['content'], 0, 300); 
+                                    echo $rest;
+                                ?></p>
+                        </div>
+                        <div class="post-main__view ">
+                            <!-- <a href="index.php?action=deletePost&id=<?= $data['id']; ?>"><i class="far fa-trash-alt text-danger mr-2"></i></a>
+                            <a href="index.php?action=postAdmin&id=<?= $data['id']; ?>"><i class="far fa-edit text-primary"></i></a> -->
+                            <a href="index.php?action=postAdmin&id=<?= $data['id']; ?>"><i class="far fa-eye text-info"></i></a>
                         </div>
                     </div>
                 </div>
