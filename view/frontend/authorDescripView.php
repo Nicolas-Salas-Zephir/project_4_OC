6479c02f9ab49295e50e072a1dfc72e3388005d8
Nicolas Salas-Zephir <znco93@hotmail.fr>
2020-05-09 16:39:29 +0200

    Create the author biography page

---
+++ view/frontend/authorDescripView.php
@@ -0,0 +1,46 @@
+<?php 
+session_start();
+$title = 'Mon blog'; 
+ob_start(); 
+?>  
+        <div class="row d-flex justify-content-center mt-5 mb-5 author-description">
+                <div class="col-10 col-md-8">
+                        <h1 class="mb-5">Biographie</h1>
+                        <p class="mb-5">Muir est né le 21 avril 1838 à Dunbar, dans l'East Lothian, en Écosse. Ses parents se nomment Daniel Muir et Ann Gilrye. Il est un de leurs huit enfants, précédé par Margaret et Sarah, et suivi par David, Daniel, Ann et Mary (jumelles) et Joanna, née aux États-Unis. Dans son autobiographie, il raconte ses passe-temps d'enfance : bagarres au cours desquelles il rejouait les batailles épiques des Guerres d'indépendance de l'Histoire Écossaise, ou bien concours de découverte de nids d'oiseaux. De tels passe-temps se révéleront formateurs pour le caractère de Muir, une fois adulte.
+
+
+                        L'entrée de la ferme Fountain Lake Farm près de Portage.
+
+                        La vallée de Yosemite, vue à partir d'Inspiration Point.
+
+                        Glacier de la Stickeen Valley (Alaska), photo extraite du livre Alaska Days with John Muir, écrit par Samuel Hall Young, publié en 1915.
+                        Muir émigre aux États-Unis en 1849, lorsque sa famille acquiert une ferme non loin de Portage, dans le Wisconsin, appelée Fountain Lake Farm. Il entre à l'université du Wisconsin à Madison en 1860 où il suit des cours pendant quelques années. C'est là-bas, sous un haut caroubier que Muir suit son premier cours de botanique. Un de ses amis étudiant cueille une fleur de l'arbre et explique que le grand caroubier fait partie de la famille des pois, plus particulièrement à celle des pois non alignés. Cinquante ans plus tard, le naturaliste décrit ce moment dans son autobiographie. Ce petit cours m'a envouté et m'a fait voler par-delà les forêts et les prairies avec un enthousiasme sauvage, écrit Muir. Mais au lieu de passer sa licence dans une école construite par la main de l'homme, Muir opte pour s'inscrire à « l'université de la vie sauvage » et parcourt ainsi des centaines de kilomètres, de l'Indiana à la Floride, après avoir passé les années 1866 et 1867 à travailler en tant qu'ingénieur industriel à Indianapolis, où il perd presque la vue dans un accident du travail. Il avait prévu de continuer à voyager en Amérique du Sud, mais il est atteint de paludisme, et se rend donc en Californie.
+
+                        Arrivé à San Francisco en mars 1868, Muir part immédiatement à la recherche d'un lieu dont il ne connaissait que le nom, Yosemite. Découvrant la vallée de Yosemite, il est captivé et écrit Aucun temple construit de la main de l'homme ne peut être comparé à Yosemite, et Yosemite est le plus grand de tous les temples dédiés à la Nature.
+
+                        Après un premier séjour d'une semaine, il retourne dans les montagnes de la Sierra Nevada et devient conducteur de ferry, berger et amateur de rodéo. En mai 1869, un propriétaire de ranch nommé Pat Delaney lui propose un travail d'été, consistant à mener et surveiller son troupeau de moutons. Muir accepte l'offre avec enthousiasme, et passe l'été dans la région de Yosemite. Ce même été, il escalade le pic Cathedral, le mont Dana et fait une marche le long d'une vieille piste Indienne du Bloody Canyon au lac Mono. En même temps il commence à travailler sur des théories concernant la façon dont la région s'est développée et dont son écosystème fonctionne.
+
+                        Encore plus enthousiaste sur la région qu'avant, Muir trouve un emploi dans une scierie dans la vallée de Yosemite, sous la direction de James Madison Hutchings. Inventeur né, Muir dessine un moulin à eau pour couper les arbres mis à terre par le vent et se construit une petite cabane le long de la Yosemite Creek.
+
+                        Son intérêt pour les sciences, plus spécialement pour la géologie, occupe son temps libre et il acquiert bientôt la conviction que les glaciers ont sculpté la vallée de Yosemite et ses environs. Cette intuition est en complète contradiction avec la théorie alors en vigueur, promulguée par Josiah Whitney (à la tête du California Geological Survey), qui attribue la formation de la Vallée à un tremblement de terre catastrophique. Les idées de Muir se propageant, Whitney tente de discréditer Muir en le traitant d'amateur, et même d'ignorant. Le premier géologue, Louis Agassiz, quant à lui, trouve de l'intérêt dans les théories de Muir, et le désigne comme « le premier homme à avoir une conception juste de l'action des glaciers ».
+
+                        En 1871, Muir découvre un glacier alpin en activité derrière Merced Peak, ce qui aide profondément à la prise en considération de ses théories. Il ést aussi un écrivain très productif, un grand nombre de ses livres sont publiés jusqu'à New York. De plus, cette année-là, l'un des héros de Muir, Ralph Waldo Emerson, arrive à Yosemite et cherche à le rencontrer. L'ancien professeur de Muir à l'université du Wisconsin, Ezra Carr, et la femme de Carr, l'encouragent à publier ses théories. Ils le présentent à des notables comme Emerson, ainsi qu'à d'éminents scientifiques comme Louis Agassiz, John Tyndall, John Torrey, Clinton Hart Merriam et Joseph LeConte.
+
+
+                        La maison de John Muir à Martinez, initialement celle de son beau-père John Theophil Strentzel.
+                        Un important tremblement de terre centré à côté de Lone Pine, en Californie, dans la vallée de l'Owens est fortement ressenti dans la vallée de Yosemite en mars 1872. Le séisme réveille Muir à l'aube, il sort de sa cabane en courant, à la fois heureux et effrayé, en s'exclamant « un noble tremblement de terre ! ». Les colons des autres vallées, qui continuent d'adhérer aux idées de Whitney, sont effrayés à l'idée que le séisme puisse être le prélude d'un catastrophique approfondissement de la vallée. Muir n'a pas la même peur, et se met, à la lueur de la lune, à la recherche de nouveaux talus créés par les éboulements provoqués par le tremblement de terre. Cet événement entraîne de plus en plus de monde à se rattacher aux idées de Muir à propos de la formation de la vallée.
+
+                        En complément de ses études de géologie, Muir enquête aussi sur la botanique dans la région de Yosemite. Il suit deux spécialisations, la répartition du séquoia géant le long du flanc ouest de la Sierra Nevada, et l'écologie de ses bosquets isolés, en 1873 et 1874. En 1876 l'Association Américaine pour le Progrès de la Science publie une note de Muir à propos de la répartition et de l'écologie des arbres.
+
+                        En 1880, Muir épouse Louisa Wanda Strentzel, dont les parents, le docteur John Strentzel (en) et sa femme Louisiana Erwin, sont propriétaires d'une grande ferme et de vergers à Martinez, une petite ville de Californie, au nord-est de San Francisco. Pendant les dix années suivantes, il s'occupe de la gestion de la ferme familiale, soit 2 600 hectares de vergers et de vignes qui deviennent très prospères. (À sa mort, il laisse une propriété de 250 000 $, qui valait plus de quatre millions de dollars en 2005. La maison et une partie de la ferme sont maintenant Site Historique National). Durant ces années deux filles voient le jour, Wanda et Helen.
+                        </p>
+                        <dl>
+                                <dt>Ces informations ont été récupérées sur le site de Wikipedia.</dt>
+                                <dd><a href="https://fr.wikipedia.org/wiki/John_Muir" target="_blank">John Muir étant le vrai auteur de "Voyage en Alaska".</a></dd>
+                        </dl>
+                </div>
+
+        </div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('templateFrontend.php'); ?>
